b218e7bd26801b878f05913b61f54b8b938dd15c
SimithWang <40049710+SimithWang@users.noreply.github.com>
2024-05-27 09:59:47 +0800

    Add files via upload

---
+++ Comfyui_RenameImage/__init__.py
@@ -0,0 +1,7 @@
+from .RenameImageNode import RenameImageNode
+
+NODE_CLASS_MAPPINGS = { "RenameImageNode": RenameImageNode }
+
+NODE_DISPLAY_NAME_MAPPINGS = { "FirstNode": "My First Node" }
+
+all = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
